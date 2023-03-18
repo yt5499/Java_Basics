7cd6faf563783ca0de233c7268ad75b5f5c05705
Yash Thakur <107828132+yt5499@users.noreply.github.com>
2023-03-18 16:07:59 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class RandomNumber {
+
+	public static void main(String[] args) {
+		
+		// Generating random numbers  
+		System.out.println("1st Random Number: " + Math.random());   
+		System.out.println("2nd Random Number: " + Math.random());  
+		System.out.println("3rd Random Number: " + Math.random());   
+		System.out.println("4th Random Number: " + Math.random());   
+
+	}
+
+}
