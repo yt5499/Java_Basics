4058ff4b9763772d7bae00ffe7fd2fa5f14d4e83
Yash Thakur <107828132+yt5499@users.noreply.github.com>
2023-03-19 10:44:34 +0530

    Add files via upload

---
+++ PrintASCII.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class PrintASCII {
+	
+	public static void main(String[] args)   
+	{  
+	// character whose ASCII value to be found  
+		char ch1 = 'A';  
+		char ch2 = 'b';  
+	// variable that stores the integer value of the character  
+		int asciivalue1 = ch1;  
+		int asciivalue2 = ch2;  
+		System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+		System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+	}  
+
+}
