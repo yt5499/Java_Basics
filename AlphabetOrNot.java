a1c6ac659f7616ad3c5b44d3b779e2c9a2e78c71
Yash Thakur <107828132+yt5499@users.noreply.github.com>
2023-03-20 22:56:10 +0530

    Add files via upload

---
+++ AlphabetOrNot.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class AlphabetOrNot {
+	
+    public static void main(String[] args) {
+
+        char c = 'l';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+
+}
