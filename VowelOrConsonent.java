db64cd3d08ebd63bd5946615947206f6e57234d8
Yash Thakur <107828132+yt5499@users.noreply.github.com>
2023-03-22 05:47:03 +0530

    Add files via upload

---
+++ VowelOrConsonent.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class VowelOrConsonent {
+
+	public static void main(String[] args) {
+		
+		char c = 'y';
+		
+		if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+			System.out.println(c+" is a vowel");
+		}
+		else {
+			System.out.println(c+" is a consonent");
+		}
+
+	}
+
+}
