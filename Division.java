4d21677e9d02461b15d76f2e2383f12997a9d8d0
Yash Thakur <107828132+yt5499@users.noreply.github.com>
2023-03-25 22:37:20 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		int dividend = 99, divisor = 8;
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+
+	}
+
+}
